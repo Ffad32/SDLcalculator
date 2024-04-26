d69f52b0bf0743ccff5943b6b3e1c71638b06aeb
fju1 <daniel.kratky357@gmail.com>
2024-04-26 15:07:17 +0200

    mouseinput

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main() {
     }
 
     // Create a window
-    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 576, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (window == NULL) {
         fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
         SDL_Quit();
@@ -31,6 +31,7 @@ int main() {
     const Uint8* state = SDL_GetKeyboardState(NULL);
 
     SDL_Rect button[10] = {
+        (SDL_Rect){50, 350, 60, 40},
         (SDL_Rect){50, 300, 60, 40},
         (SDL_Rect){120, 300, 60, 40},
         (SDL_Rect){190, 300, 60, 40},
@@ -40,8 +41,9 @@ int main() {
         (SDL_Rect){50, 200, 60, 40},
         (SDL_Rect){120, 200, 60, 40},
         (SDL_Rect){190, 200, 60, 40},
-        (SDL_Rect){50, 350, 60, 40}
-    };
+    };  
+
+    
 
     SDL_Event event;
     bool quit = false;
@@ -52,18 +54,39 @@ int main() {
             }
         }
 
+        Uint32 state_m = SDL_GetMouseState(NULL, NULL);
         // Set render color to blue
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         // Clear the window with the render color
         SDL_RenderClear(renderer);
 
         // Render
-    for(int i = 0; i < 10; i++){
-        SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-        SDL_RenderFillRect(renderer, &button[i]);
-    }
+        for (int i = 0; i < 10; i++) {
+            int x, y;
+            SDL_GetMouseState(&x, &y);
+            if (x >= button[i].x && y >= button[i].y && x <= (button[i].x + button[i].w) && y <= (button[i].y + button[i].h)) {
+                SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+                SDL_RenderFillRect(renderer, &button[i]);
 
-        
+                    static bool isFKeyPressed = false;
+                    if(state_m & SDL_BUTTON(SDL_BUTTON_LEFT) && !isFKeyPressed){
+                        isFKeyPressed = true;
+                        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                        SDL_RenderFillRect(renderer, &button[i]);
+                        printf("Button %d clicked\n", i);   
+                        }
+
+                        if(!state_m & SDL_BUTTON(SDL_BUTTON_LEFT)){
+
+                        isFKeyPressed = false;
+                        }
+
+            } else {
+                SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+                SDL_RenderFillRect(renderer, &button[i]);
+            }
+        }
+
         // Update the screen
         SDL_RenderPresent(renderer);
 
