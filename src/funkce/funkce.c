20a3869281b20979b413d473ecbc8cc40a1f0123
Ffad32 <frantisek.nevoral@gmail.com>
2024-06-11 20:40:07 +0200

    jis gay

--- src/funkce/funkce.c
+++ src/funkce/funkce.c
@@ -8,17 +8,20 @@ void add(int a, int b,int *result) {
 }
 
 // subtraction
-void sub() {
+void sub(int a, int b,int *result) {
+    *result= a - b;
     printf("sub\n");
 }
 
 // multiplication
-void mul() {
+void mul(int a, int b,int *result) {
+    *result= a * b;
     printf("mul\n");
 }
 
 // division
-void div() {
+void div(int a, int b,int *result) {
+    *result= a / b;
     printf("div\n");
 }
 
